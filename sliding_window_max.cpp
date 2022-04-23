d6da5641025812394eec81471ed5e741cafcd939
rayfish5 <96719751+rayfish5@users.noreply.github.com>
2022-04-22 22:18:04 -0700

    Create sliding_window_max.cpp

---
+++ sliding_window_max.cpp
@@ -0,0 +1,14 @@
+vector<int> nums; // the array
+int k;            // sliding window size
+
+deque<int> mono;  // decreasing deque of maxes
+vector<int> ret;  // stores sliding window maxes
+
+for (int i = 0; i < nums.size(); i++) {
+  if (i >= k && mono.front() == nums[i-k]) mono.pop_front();
+  while (!mono.empty() && mono.back() < nums[i]) {
+    mono.pop_back();
+  }
+  mono.push_back(nums[i]);
+  if (i >= k-1) ret.push_back(mono.front());
+}
