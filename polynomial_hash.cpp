d89d6d603579799b268fb721b026cd35abf6c801
rayfish5 <96719751+rayfish5@users.noreply.github.com>
2022-06-08 11:44:50 -0700

    Create polynomial_hash.cpp

---
+++ polynomial_hash.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+struct HashedString {
+  vector<ll> pow {1};
+  vector<ll> p_hash;
+  const ll M;
+  const ll P;
+  HashedString(const string& s, ll m, ll p) : p_hash(s.size()+1), M(m), P(p) {
+    while (pow.size() <= s.size()) {
+      pow.push_back((pow.back() * P) % M);
+    }
+    p_hash.at(0) = 0;
+    for (int i = 0; i < s.size(); i++) {
+      p_hash.at(i+1) = ((p_hash.at(i) * P) % M + s.at(i)) % M;
+    }
+  }
+  ll get_hash(int b, int e) {
+    ll raw_val = p_hash.at(e+1) - p_hash.at(b) * pow.at(e-b+1);
+    return (raw_val % M + M) % M;
+  }
+};
