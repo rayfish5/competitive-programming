2d79696cbb6534a756e42d184201c068b4d943f0
rayfish5 <96719751+rayfish5@users.noreply.github.com>
2022-06-25 17:14:02 -0700

    Create linear_sieve.cpp

---
+++ linear_sieve.cpp
@@ -0,0 +1,12 @@
+const int MAX_N = 5e8;
+vector<int> p; // primes
+bool comp[MAX_N+1]; // initial assume prime
+void sieve(int n) {
+  for (int i = 2; i <= n; i++) {
+    if (!comp[i]) p.push_back(i);
+    for (int j = 0; j < p.size() && i * p[j] <= n; j++) {
+      comp[i * p[j]] = true;
+      if (i % p[j] == 0) break;
+    }
+  }
+}
