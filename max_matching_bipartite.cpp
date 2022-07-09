5843229c7c02cf9225518a7001f868620e2a25ca
rayfish5 <96719751+rayfish5@users.noreply.github.com>
2022-07-09 12:39:09 -0700

    Update max_matching_bipartite.cpp

--- max_matching_bipartite.cpp
+++ max_matching_bipartite.cpp
@@ -4,22 +4,22 @@ using namespace std;
 typedef long long ll;
 
 // nodes numbered 1 - n
-bool dfs(int cur, int n, vector<int>* adj, bool* vis, int* match) {
+bool dfs(int cur, vector<int>* adj, bool* vis, int* match) {
   if (vis[cur]) return false;
   vis[cur] = true;
-  for (auto nxt : adj[cur]) if (!match[nxt] || dfs(match[nxt], n, adj, vis, match)) {
+  for (auto nxt : adj[cur]) if (!match[nxt] || dfs(match[nxt], adj, vis, match)) {
     match[nxt] = cur;
     return true;
   }
   return false;
 }
-int max_matching(int n, vector<int>* adj) {
-  int match[n+1] {}; // for right nodes
-  bool vis[n+1];
+int max_matching(int l_cnt, int r_cnt, vector<int>* adj) {
+  int match[r_cnt+1] {}; // for right nodes
+  bool vis[l_cnt+1];
   int max_match = 0;
-  for (int left = 1; left <= n; left++) {
-    fill_n(vis, n+1, false);
-    max_match += dfs(left, n, adj, vis, match);
+  for (int left = 1; left <= l_cnt; left++) {
+    fill_n(vis, l_cnt+1, false);
+    max_match += dfs(left, adj, vis, match);
   }
   return max_match;
 }
