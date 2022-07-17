9331fac3233a1e8861eeba259ec7588f8cf25742
rayfish5 <96719751+rayfish5@users.noreply.github.com>
2022-07-16 18:28:30 -0700

    Create bit.cpp

---
+++ bit.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+struct BIT {
+    int n;
+    vector<ll> arr;
+    // BIT with only 0's
+    BIT(int _n) : n(_n) {
+        arr = vector<ll> (n+1);
+    }
+    // 1-indexed
+    void update(int i, ll inc) {
+        for (; i <= n; i += (i & -i))
+            arr[i] += inc;
+    }
+    // prefix sum
+    ll psum(int i) {
+        ll s = 0;
+        for (; i > 0; i -= (i & -i))
+            s += arr[i];
+        return s;
+    }
+};
