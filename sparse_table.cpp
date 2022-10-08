9f74ec5b1281c8f6f210de7dc16490ae954df57d
rayfish5 <96719751+rayfish5@users.noreply.github.com>
2022-10-08 10:44:28 -0700

    Update sparse_table.cpp

--- sparse_table.cpp
+++ sparse_table.cpp
@@ -1,10 +1,10 @@
-const int MAX_N = 1<<18;
-const int LOG_N = 19;
+const int MAX_N = 1<<19;
+const int LOG_N = 20;
 ll stmax[LOG_N][MAX_N];
 ll stmin[LOG_N][MAX_N];
 
 void build_st(vector<ll>& arrmax, vector<ll>& arrmin, int n) {
-    int h = floor(log2(n));
+    int h = __lg(n);
     for (int j = 0; j < n; j++) {
         stmax[0][j] = arrmax[j];
         stmin[0][j] = arrmin[j];
@@ -18,10 +18,10 @@ void build_st(vector<ll>& arrmax, vector<ll>& arrmin, int n) {
 }
 // 0-indexed, [l, r)
 ll r_max(int l, int r) {
-    int p = 31 - __builtin_clz(r-l);
+    int p = __lg(r-l);
     return max(stmax[p][l], stmax[p][r-(1<<p)]);
 }
 ll r_min(int l, int r) {
-    int p = 31 - __builtin_clz(r-l);
+    int p = __lg(r-l);
     return min(stmin[p][l], stmin[p][r-(1<<p)]);
 }
