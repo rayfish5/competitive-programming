eedac6f3fa0f9f94f767b3fbe88642595d552e2f
rayfish5 <96719751+rayfish5@users.noreply.github.com>
2024-01-15 19:46:45 -0800

    Update factorials.cpp
    implemented faster O(n + log m) solution

--- factorials.cpp
+++ factorials.cpp
@@ -1,5 +1,5 @@
-// 0 <= b < MOD
 ll bpow(ll b, ll e) {
+  b %= MOD;
   ll res = 1;
   while (e > 0) {
     if (e & 1) res = res * b % MOD;
@@ -17,10 +17,13 @@ vector<ll> fact (MAX_A+1);
 vector<ll> fact_inv (MAX_A+1);
 
 void setup() {
-  fact[0] = fact_inv[0] = 1;
+  fact[0] = 1;
   for (int i = 1; i <= MAX_A; i++) {
     fact[i] = fact[i-1] * i % MOD;
-    fact_inv[i] = inv(fact[i]);
+  }
+  fact_inv[MAX_A] = inv(fact[MAX_A]);
+  for (int i = MAX_A; i > 0; i--) {
+    fact_inv[i-1] = fact_inv[i] * i % MOD;
   }
 }
 ll binom(int a, int b) {
